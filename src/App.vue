c9a3d774e3996f9c7d3a1c9337826c7a20d7c9c6
erfank245 <erfank245@gmail.com>
2024-09-18 15:38:47 +0330

    init

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <nav>
+      <router-link
+        class="logo-link"
+        to="/"
+      >
+        <img
+          class="logo"
+          alt="logo"
+          src="https://graphicallyapp.ir/MainLogo.png"
+        />
+        <div class="product-name">میکرو-فرانت اند</div>
+      </router-link>
+      <router-link
+        class="nav-link"
+        active-class="active-nav-link"
+        to="/link-1"
+      >
+        لینک ۱
+      </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    links: Array,
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  direction: rtl;
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  height: 80px;
+  padding: 0 calc(calc(100vw - 700px) / 2);
+  background-color: white;
+  transition: left 0.2s ease-in-out;
+  z-index: 1000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+  box-shadow: 0 0.2rem 1rem #00000040;
+}
+.navbar-links {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+.logo-link {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+}
+.logo-link .logo {
+  height: 60px;
+}
+.logo-link .product-name {
+  color: black;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+.nav-link {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: black;
+}
+.active-nav-link {
+  font-weight: 600;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import "./set-public-path";
+import singleSpaVue from 'single-spa-vue';
+import { createRouter,createWebHistory } from "vue-router";
+import { h, createApp } from "vue";
+
+import App from "./App.vue";
+
+const routes = [
+  { path: "/" },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+const vueLifecycles = singleSpaVue({
+  createApp,
+  appOptions: {
+    render() {
+      return h(App, {
+        links: this.links,
+      });
+    },
+  },
+  handleInstance: (app) => {
+    app.use(router);
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
